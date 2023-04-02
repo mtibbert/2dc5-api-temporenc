9406fcd979137556d96467a99e049eeaddccb9d1
github.v <github.v@qneni.com>
2023-04-02 16:21:05 -0500

    refactor (encode): Extract as Class #5
    Extract encode endpoint to class. Renamed source code directory to
    avoid confusion.

--- runner.py
+++ runner.py
@@ -1,9 +1,9 @@
 import config
-from app import api
+from src import app
 
 
 def run():
-    api.run(host=config.ADDR, port=config.PORT, debug=False)
+    app.run(host=config.ADDR, port=config.PORT, debug=False)
 
 
 if __name__ == '__main__':
