1d1b27d1579dd6556ee0783f03d0ec8d19f827e9
github.v <github.v@qneni.com>
2023-04-07 09:16:39 -0500

    fix (encode_bp): Add encode_bp.py #15

---
+++ src/routes/encode/encode_bp.py
@@ -0,0 +1,22 @@
+from flask import Blueprint
+
+encode_bp = Blueprint('encode', __name__)
+
+
+@encode_bp.route("/encode/<iso_string>", methods=["GET"])
+def do_encode_iso(iso_string: str) -> object:
+    """
+    Encode ISO string
+
+    Returns uppercase hexadecimal Temporenc encoded string # noqa: E501
+
+    :param iso_string: ISO string to encode
+    :type iso_string: str
+
+    :rtype: Codec
+    """
+    return {
+        'decoded': iso_string[::-1],
+        'encoded': iso_string,
+        'precision': "PRECISION_MILLI",
+        'type': 'TYPE_DTS'}
