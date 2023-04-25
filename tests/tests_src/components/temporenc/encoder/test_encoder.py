f6e77b7460cdc0d6723c14b6cff0d2408738c7f5
github.v <github.v@qneni.com>
2023-04-25 16:43:41 -0500

    refactor (TestEncoder): Superclass dataprovider
    Related: #21

--- tests/tests_src/components/temporenc/encoder/test_encoder.py
+++ tests/tests_src/components/temporenc/encoder/test_encoder.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from tests_src.components.temporenc.encoder.tests_encoder_base import TestsEncoderBase
 from components.parsers.parse_iso_strings import Parse
 from components.temporenc import Encoder, Utilities
 from type_ext import IsoDict
@@ -6,37 +6,10 @@ from type_ext.temporenc_arg_dict import TemporencArgDict
 import datetime
 
 
-class TestEncoder(TestCase):
-
-    data_provider = {}
+class TestEncoder(TestsEncoderBase):
 
     def setUp(self) -> None:
-        data = {
-            "TYPE_D": [
-                {"iso": "1983-01-15",
-                 "encoded": "8F7E0E"}],
-            "TYPE_T": [
-                {"iso": "18:25:12",
-                 "encoded": "A1264C"}],
-            "TYPE_DT": [
-                {"iso": "1983-01-15T18:25:12",
-                 "encoded": "1EFC1D264C"}],
-            "TYPE_DTS": [
-                {"iso": "2023-03-28T17:27:00.123420",
-                 "encoded": "57E72DC5B0078870"},
-                {"iso": "1983-01-15T18:25:12.123456",
-                 "encoded": "57BF074993078900"}],
-            "TYPE_DTZ": [
-                {"iso": "1983-01-15T18:25:12+01:00",
-                 "encoded": "CF7E0E8B2644"}],
-            "TYPE_DTSZ": [
-                {"iso": "1983-01-15T18:25:12.123456+01:00",
-                 "encoded": "EBDF83A2C983C48110"}],
-        }
-        self.dp_complex_types = \
-            data["TYPE_DTSZ"] + data["TYPE_DTZ"] + data["TYPE_DTS"] + data["TYPE_DT"]
-        self.dp_elemental_types = data["TYPE_D"] + data["TYPE_T"]
-        self.data_provider = self.dp_complex_types + self.dp_elemental_types
+        super().setUp()
 
     def test_encode_by_args_complex_types(self):
         for item in self.dp_complex_types:
