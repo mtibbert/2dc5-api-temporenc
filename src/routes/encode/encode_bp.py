25f410f0f8b1bda996bedc0e0e15139fbf1e4b97
github.v <github.v@qneni.com>
2023-05-02 12:52:18 -0500

    fea (routes.encode): Initial encode conversion #42
    Encode route response properly. Updated tests to utilize dataprovider.

--- src/routes/encode/encode_bp.py
+++ src/routes/encode/encode_bp.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
 
+from components.temporenc import Encoder
+from type_ext.precision_type import PrecisionType
+from type_ext.temporenc_type import TemporencType
+
 encode_bp = Blueprint('encode', __name__)
 
 
@@ -15,8 +19,11 @@ def do_encode_iso(iso_string: str) -> object:
 
     :rtype: Codec
     """
+    encoded = Encoder.encode_iso_str(iso_str=iso_string)
+    precision = PrecisionType.encoded_to_precision_type(encoded_str=encoded).name
+    type_ext = TemporencType.type_of(encoded=encoded).name
     return {
-        'decoded': iso_string[::-1],
-        'encoded': iso_string,
-        'precision': "PRECISION_MILLI",
-        'type_ext': 'TYPE_DTS'}
+        'decoded': iso_string,
+        'encoded': encoded,
+        'precision': precision,
+        'type_ext': type_ext}
