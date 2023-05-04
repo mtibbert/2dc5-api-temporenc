e6ab522ff8b57d67e12521035d21da0aedb56b93
github.v <github.v@qneni.com>
2023-05-04 09:13:39 -0500

    chore (routes): Added TODOs
    Issue: #43, #49

--- src/routes/decode/decode_bp.py
+++ src/routes/decode/decode_bp.py
@@ -19,6 +19,7 @@ def do_decode_encoded_str(encoded_str: str) -> object:
 
     :rtype: Codec
     """
+    # #49 Malformed URL Arg Should Return 404 Status Code
     precision = PrecisionType.encoded_to_precision_type(encoded_str=encoded_str).name
     type_ext = TemporencType.type_of(encoded=encoded_str).name
     match type_ext:
--- src/routes/encode/encode_bp.py
+++ src/routes/encode/encode_bp.py
@@ -19,6 +19,7 @@ def do_encode_iso(iso_string: str) -> object:
 
     :rtype: Codec
     """
+    # #49 Malformed URL Arg Should Return 404 Status Code
     encoded = Encoder.encode_iso_str(iso_str=iso_string)
     precision = PrecisionType.encoded_to_precision_type(encoded_str=encoded).name
     type_ext = TemporencType.type_of(encoded=encoded).name
