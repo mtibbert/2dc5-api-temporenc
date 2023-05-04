25bb02d68241857c427fc349648c2570271f0ea9
github.v <github.v@qneni.com>
2023-05-04 15:47:28 -0500

    fea (routes.decode): Implement 404 Response
    Invalid encoded value (ones not recognized as Temporenc encoded) and
    non-hex values return json response. The message property contains the
    provided value. Invalid encoded value will also identify the Temporenc
    type identified in by hex string.
    
    API schema updated.
    
    Issue: #43, #49

--- src/routes/decode/decode_bp.py
+++ src/routes/decode/decode_bp.py
@@ -20,16 +20,22 @@ def do_decode_encoded_str(encoded_str: str) -> object:
     :rtype: Codec
     """
     # #49 Malformed URL Arg Should Return 404 Status Code
-    precision = PrecisionType.encoded_to_precision_type(encoded_str=encoded_str).name
-    type_ext = TemporencType.type_of(encoded=encoded_str).name
-    match type_ext:
-        case TemporencType.TYPE_D.name:
-            fn = Decoder.as_date
-        case TemporencType.TYPE_T.name:
-            fn = Decoder.as_time
-        case _:
-            fn = Decoder.as_date_time
-    decoded = fn(hex_str=encoded_str).isoformat()
+    try:
+        # Possible ValueError("invalid literal for int() with base 16: '...'")
+        precision = PrecisionType.encoded_to_precision_type(encoded_str=encoded_str).name
+        type_ext = TemporencType.type_of(encoded=encoded_str).name
+        match type_ext:
+            case TemporencType.TYPE_D.name:
+                fn = Decoder.as_date
+            case TemporencType.TYPE_T.name:
+                fn = Decoder.as_time
+            case _:
+                fn = Decoder.as_date_time
+        # Possible ValueError('"..." not recognized as TYPE_x')
+        decoded = fn(hex_str=encoded_str).isoformat()
+    except ValueError as ve:
+        err_msg = str(ve).replace('"', "'")
+        return {"message": f'Resource not found: {err_msg}'}, 404
     return {
         'decoded': decoded,
         'encoded': encoded_str,
--- tests/tests_src/routes/decode/test_decode.py
+++ tests/tests_src/routes/decode/test_decode.py
@@ -12,24 +12,6 @@ class TestDecodeGet(TestsBase):
         super().setUp()
         self.app = app.test_client()
 
-    def test_response_is_json(self):
-        for item in self.data_provider:
-            response = get_response(test_client=self.app,
-                                    prefix=self.prefix_de,
-                                    route_arg=item["encoded"])
-            with self.subTest(f'{self.prefix_de}{item["encoded"]} returns JSON content'):
-                self.assertTrue(response.is_json)
-
-    def test_response_status_code_is_200(self):
-        expected = 200
-        for item in self.data_provider:
-            response = get_response(test_client=self.app,
-                                    prefix=self.prefix_de,
-                                    route_arg=item["encoded"])
-            with self.subTest(f'{self.prefix_de}{item["encoded"]} returns status code '
-                              f'{expected}'):
-                self.assertEqual(expected, response.status_code)
-
     def test_decodes_hex_str_correctly(self):
         for item in self.data_provider:
             results = dp_item_actual_expected_dict(test_client=self.app,
@@ -40,7 +22,7 @@ class TestDecodeGet(TestsBase):
                               f'data'):
                 self.assertDictEqual(results["expected"], results["actual"])
 
-    def test_decodes_hex_str__equals_encoded_iso_str(self):
+    def test_decodes_hex_str_equals_encoded_iso_str(self):
         for item in self.data_provider:
             encoded_response = get_response(test_client=self.app,
                                             prefix=self.prefix_en,
@@ -51,3 +33,111 @@ class TestDecodeGet(TestsBase):
             with self.subTest(f'/decode/{item["encoded"]} response == ' +
                               f'/encode/{item["iso"]}'):
                 self.assertDictEqual(decoded_response.json, encoded_response.json)
+
+    def test_response_200_response_type_is_json(self):
+        for item in self.data_provider:
+            response = get_response(test_client=self.app,
+                                    prefix=self.prefix_de,
+                                    route_arg=item["encoded"])
+            with self.subTest(f'{self.prefix_de}{item["encoded"]} returns JSON content'):
+                self.assertTrue(response.is_json)
+
+    def test_response_200_status_code_for_valid_encoded_value(self):
+        expected = 200
+        for item in self.data_provider:
+            response = get_response(test_client=self.app,
+                                    prefix=self.prefix_de,
+                                    route_arg=item["encoded"])
+            with self.subTest(f'{self.prefix_de}{item["encoded"]} returns status code '
+                              f'{expected}'):
+                self.assertEqual(expected, response.status_code)
+
+    def test_response_404_response_json_key_message_is_str(self):
+        data_provider = ["x3F396E0D803DC40"]
+        expected = ["message"]
+
+        for route_arg in data_provider:
+            response = get_response(test_client=self.app,
+                                    prefix=self.prefix_de,
+                                    route_arg=route_arg)
+            with self.subTest(f'{self.prefix_de}{route_arg} response key(s) '
+                              f'are {expected}'):
+                self.assertListEqual(expected, list(response.json.keys()))
+                self.assertIsInstance(response.json[expected[0]], str)
+
+    def test_response_404_response_type_is_json(self):
+        data_provider = ["x3F396E0D803DC40"]
+        expected = "application/json"
+
+        for route_arg in data_provider:
+            response = get_response(test_client=self.app,
+                                    prefix=self.prefix_de,
+                                    route_arg=route_arg)
+            with self.subTest(f'{self.prefix_de}{route_arg} content_type is '
+                              f'"{expected}"'):
+                self.assertEqual(expected, response.content_type)
+
+    def test_response_404_status_code_for_invalid_encoded_value(self):
+        expected = 404
+        for item in self.data_provider:
+            response = get_response(test_client=self.app,
+                                    prefix=self.prefix_de,
+                                    route_arg=f'b{item["encoded"][1:]}')
+            with self.subTest(f'{self.prefix_de}{item["encoded"]} returns status code '
+                              f'{expected}'):
+                self.assertEqual(expected, response.status_code)
+
+    def test_response_404_status_code_for_non_hex_arg(self):
+        expected = 404
+        for item in self.data_provider:
+            arg_proxy = f'z{item["encoded"][1:]}'
+            response = get_response(test_client=self.app,
+                                    prefix=self.prefix_de,
+                                    route_arg=arg_proxy)
+            with self.subTest(f'{self.prefix_de}{arg_proxy} returns status code '
+                              f'{expected}'):
+                self.assertEqual(expected, response.status_code)
+
+    def test_response_404_err_msg_ends_with_expected_type(self):
+        expected = 404
+        for item in self.data_provider:
+            arg_proxy = f'b{item["encoded"][1:]}'
+            expected_msg_end_rx = r'not recognized as TYPE_' \
+                                  r'[D | T | DT| DTS | DTZ | DTSZ]$'
+            response = get_response(test_client=self.app,
+                                    prefix=self.prefix_de,
+                                    route_arg=arg_proxy)
+            with self.subTest(f'{self.prefix_de}{arg_proxy} ends with "TYPE_x"'):
+                self.assertEqual(expected, response.status_code)  # Check for 404
+                self.assertRegex(response.json["message"], expected_msg_end_rx)
+
+    def test_response_404_err_msg_includes_encoded_hex(self):
+        expected = 404
+        for item in self.data_provider:
+            arg_proxy = f'b{item["encoded"][1:]}'
+            expected_msg_start = f'Resource not found: \'{arg_proxy}\' ' \
+                                 f'not recognized as TYPE_'
+            response = get_response(test_client=self.app,
+                                    prefix=self.prefix_de,
+                                    route_arg=arg_proxy)
+            with self.subTest(f'{self.prefix_de}{arg_proxy} error message '
+                              f'includes {arg_proxy}'):
+                self.assertEqual(expected, response.status_code)  # Check for 404
+                self.assertTrue(
+                    str(response.json["message"]).startswith(expected_msg_start),
+                    f'expected "{response.json["message"]}" '
+                    f'to start with "{expected_msg_start}"')
+
+    def test_response_404_err_msg_non_hex_arg(self):
+        for item in self.data_provider:
+            arg_proxy = f'x{item["encoded"][1:]}'
+            expected = f'Resource not found: '\
+                       f'invalid literal for int() with base 16: '\
+                       "'" + f'{arg_proxy}' + "'"
+            response = get_response(test_client=self.app,
+                                    prefix=self.prefix_de,
+                                    route_arg=arg_proxy)
+            msg = f'"{expected[:18]} ~ {expected[-10:]}"'
+            with self.subTest(f'{self.prefix_de}{arg_proxy} message includes {msg}'):
+                self.assertEqual(404, response.status_code)  # Check for 404
+                self.assertEqual(response.json["message"], expected)
